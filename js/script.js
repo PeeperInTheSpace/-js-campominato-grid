3eee569d03f51cd1907a6b9def8b820fb76da2a1
PeeperInTheSpace <lollof94@gmail.com>
2022-09-06 18:24:27 +0200

    OK

--- js/script.js
+++ js/script.js
@@ -1,1 +1,60 @@
 console.log("JS Ok!")
+
+/*
+
+L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro se il numero progressivo è pari, di verde se dispari. Inoltre emette un messaggio in console con il numero della cella cliccata.
+
+*/
+
+const gridButton = document.getElementById("gridgenerator");
+
+gridButton.addEventListener("click", function() {
+
+    console.log("click");
+    const grid = document.getElementById("gridDom");
+    grid.innerHTML = "";
+
+    for (let i = 1; i <= 100; i++) {
+
+        const gridCell = document.createElement("div");
+        gridCell.innerHTML = i;
+        gridCell.className = "gridCell";
+        grid.append(gridCell);
+
+        console.log(i);
+
+        gridCell.addEventListener("click", function() {
+
+            console.log("click");
+
+            const isEven = checkIsEven(i);
+
+            if (isEven) {
+
+                gridCell.classList.add("evenbox");
+
+            } else {
+
+                gridCell.classList.add("oddbox");
+
+            }
+
+        });
+
+    }
+
+
+    }
+
+);
+
+function checkIsEven(number) {
+
+    return number % 2 === 0;
+
+}
+
+
